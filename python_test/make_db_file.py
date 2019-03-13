0cca16b144981c5c2d1d95b72c732ce847865983
grubby <jianyangtft@163.com>
2019-03-13 21:24:18 +0800

    test for file-action

---
+++ python_test/make_db_file.py
@@ -0,0 +1,40 @@
+dbfilename = 'people-file'
+ENDDB = 'enddb.'
+ENDREC = 'endrec.'
+RECSEP = '=>'
+
+
+def storeDbase(db, dbfilename=dbfilename):
+    '''将数据库格式化数据保存为普通文件'''
+    dbfile = open(dbfilename, 'w')
+    for key in db:
+        print(key, file=dbfile)
+        for (name, value) in db[key].items():
+            print(name + RECSEP + repr(value), file=dbfile)
+        print(ENDREC, file=dbfile)
+    print(ENDDB, file=dbfile)
+    dbfile.close()
+
+
+def loadDbase(dbfilename=dbfilename):
+    '''解析数据，重构数据库'''
+    dbfile = open(dbfilename)
+    import sys
+    sys.stdin = dbfile
+    db = {}
+    key = input()
+    while key != ENDDB:
+        rec = {}
+        field = input()
+        while field != ENDREC:
+            name, value = field.split(RECSEP)
+            rec[name] = eval(value)
+            field = input()
+        db[key] = rec
+        key = input()
+    return db
+
+
+if __name__ == '__main__':
+    from initdata import db
+    storeDbase(db)
