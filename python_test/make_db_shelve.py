136fa4e8f866ef6be7f27d8e00acd32bd98dbdc3
grubby <jianyangtft@163.com>
2019-03-14 22:54:39 +0800

    test

--- python_test/make_db_shelve.py
+++ python_test/make_db_shelve.py
@@ -1,6 +1,7 @@
 from initdata import bob, sue
 import shelve
-db = shelve.open(r'F:\Repository\sshtest\python_test\people_data\people-shelve')
+db = shelve.open(
+    r'F:\Repository\sshtest\python_test\people_data\people-shelve')
 db['bob'] = bob
 db['sue'] = sue
 db.close()
