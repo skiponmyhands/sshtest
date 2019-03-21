e619b0e7a3b2290b8ff2683d37a2ed19fe5d8916
grubby <jianyangtft@163.com>
2019-03-21 20:28:05 +0800

    test

---
+++ sys-os-test/testargv2.py
@@ -0,0 +1,17 @@
+def getopts(argv):
+    opts = {}
+    while argv:
+        if argv[0][0] == '-':
+            opts[argv[0]] = argv[1]
+            argv = argv[2:]
+        else:
+            argv = argv[1:]
+    return opts
+
+
+if __name__ == "__main__":
+    from sys import argv
+    myargvs = getopts(argv)
+    if '-i' in myargvs:
+        print(myargvs['-i'])
+    print(myargvs)
